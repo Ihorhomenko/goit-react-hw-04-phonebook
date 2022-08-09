daa40c4606efd201599c55b96e08b257e7d7e546
Ihor Khomenko <igorhomenko97@gmail.com>
2022-08-09 12:56:55 +0300

    finished hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,58 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import PhoneRegBook from './phonebook/phonebook';
 import Contacts from './contacts/contacts';
 import Filter from './filter/filter';
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
+function App () {
+ 
+  const [contacts, setContacts] = useState(() => {
+    try {
+      const savedContacts = localStorage.getItem('contacts');
+      return savedContacts === null ? undefined : JSON.parse(savedContacts);
+    } catch (error) {
+      console.error("Get state error: ", error.message);
+    }
+  })
+
+  const [filter, setFilter] = useState('')
+
+  useEffect (() => {
+    window.localStorage.setItem("contacts", JSON.stringify(contacts))
+  }, [contacts]) 
+
+  const handleSubmitForm = (data) => {
+    setContacts([...contacts, data] )
   };
 
-  componentDidMount() {
-      try {
-        const savedContacts = localStorage.getItem('contacts');
-        return savedContacts === null ? undefined : this.setState({ contacts: JSON.parse(savedContacts) });
-      } catch (error) {
-        console.error("Get state error: ", error.message);
-      }
-  }
-
-  componentDidUpdate() {
-    localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
-  }
-
-  handleSubmitForm = data => {
-    this.setState({ contacts: [...this.state.contacts, data] })
+  const handleChangeFilter = value => {
+    setFilter(value);
   };
 
-  handleChangeFilter = value => {
-    this.setState({ filter: value });
+  const normalazeFilter = filter.toLowerCase();
+  const visibleContacts = contacts.filter(contact =>
+  contact.name.toLowerCase().includes(normalazeFilter))
+
+  const handleDelete = id => {
+    setContacts(contacts.filter(contact => contact.id !== id))
   };
 
-  handleDelete = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-  };
 
-  render() {
-    const normalazeFilter = this.state.filter.toLowerCase();
-    const visibleContacts = this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalazeFilter)
-    );
-    return (
+    
+  return (
       <div>
         <section className='phonebook'>
         <h2>Phonebook</h2>
         <div className="phone-reg-book">
-          <PhoneRegBook onSubmit={this.handleSubmitForm} />
+          <PhoneRegBook onSubmit={handleSubmitForm} />
         </div>
         <div>
           <h2>Contacts</h2>
-          <Filter onFilter={this.handleChangeFilter} />
-          <Contacts contacts={visibleContacts} onDelete={this.handleDelete} />
+          <Filter onFilter={handleChangeFilter} />
+          <Contacts contacts={visibleContacts} onDelete={handleDelete} />
         </div>
         </section>
       </div>
     );
   }
-}
 
 export default App;
